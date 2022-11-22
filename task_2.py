1642baa52e2dfe7ff3d6ac8e162210994e088982
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-11-22 22:08:04 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,20 @@
 def fibonacci_sequence(n):
-    return []
+    fibonacci=[]
+    x=0
+
+    while len(fibonacci) != n:
+        if (x<3):
+            if (x==0):
+                fibonacci.append(0)
+                x+=1
+                continue
+            elif (x==1) or (x==2):
+                fibonacci.append(1)
+                x+=1
+                continue       
+        a = fibonacci[x-1]+fibonacci[x-2]
+        fibonacci.append(a)
+        x+=1
+        return fibonacci
+
+fibonacci_sequence(n)
