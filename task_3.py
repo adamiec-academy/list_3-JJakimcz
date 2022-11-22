1a97b2799d4508a62d92142c59a0f6f483c93da3
Jakimc <88589226+JJakimcz@users.noreply.github.com>
2022-11-22 22:08:24 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,1 +1,22 @@
-# Remove this comment to confirm that this task is done
+from random import randint
+from turtle import *
+import turtle
+
+t = turtle.Turtle()
+
+t.speed("fast")
+
+
+STEP = 30
+MOUNT = [0, 10, 100, 150, 60, 20, 0, 20, 40, 60, 20, 10, 0]
+for i in range(len(MOUNT)):
+    if MOUNT[i]==0:
+        goto(i * STEP,MOUNT[i])
+        continue
+    goto(i * STEP, MOUNT[i] + randint(0,20))
+
+t.home
+t.forward(400)
+
+
+exitonclick
